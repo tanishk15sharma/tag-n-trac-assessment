da50e0a0dc4dce6dd0b8d5955d4775716f0ab143
Tanishk Sharma <tanishk1582002@gmail.com>
2023-03-31 15:31:01 +0530

    style: pnd button

--- src/pages/CustomerDashboard.tsx
+++ src/pages/CustomerDashboard.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const CustomerDashboard = () => {
-  return <div>CustomerDashboard</div>;
+  return (
+    <div className="m-10 mx-20 ">
+      <button className="bg-blue-400 py-2 px-3 font-semibold text-white">
+        PICK AND DROP
+      </button>
+    </div>
+  );
 };
 
 export { CustomerDashboard };
