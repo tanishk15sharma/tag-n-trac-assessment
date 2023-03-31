25d9fa5086d3a72d61df43b5063a76405ec56284
Tanishk Sharma <tanishk1582002@gmail.com>
2023-03-31 15:22:56 +0530

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { CustomerDashboard } from "./pages/CustomerDashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="font-semibold">Assessment</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="dashboard" element={<CustomerDashboard />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
