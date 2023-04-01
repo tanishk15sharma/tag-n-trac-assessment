14f1b6301be5588a80b5fdca09b620a43f59ce28
Tanishk Sharma <tanishk1582002@gmail.com>
2023-04-01 22:41:39 +0530

    feat: delete shippment

--- src/components/ShippmentCard.tsx
+++ src/components/ShippmentCard.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-
+import axios from "axios";
+import { useUser } from "../contexts/customerContext";
 const ShippmentCard = ({ shippmentDetails }: any) => {
+  const { setUserData } = useUser();
+
+  const handleDelete = async (shippmentId: number) => {
+    const { status } = await axios.delete(
+      `http://localhost:3000/shippments/${shippmentId}`
+    );
+
+    if (status === 200) {
+      setUserData((previousData) => ({
+        ...previousData,
+        shippments: previousData?.shippments.filter(
+          (shippment: any) => shippment.id !== shippmentId
+        ),
+      }));
+    }
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <li className=" my-3 bg-gray-50  p-4 ">
       <div className="flex justify-end gap-2 mb-2 text-gray-500">
         <button>
           <span className="material-icons-outlined">edit</span>
         </button>
-        <button>
+        <button onClick={() => handleDelete(shippmentDetails.id)}>
           <span className="material-icons-outlined">delete</span>
         </button>
       </div>
